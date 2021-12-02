a306d797ff0497d5233a887d13c29e629f012d38
Chirag Dev <chirag.dev@robosoftin.com>
2021-12-03 01:38:43 +0530

    home page scroll background created

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,44 @@
+import "./Homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div className="one">
+        <h1> First Page</h1>
+      </div>
+      <div className="two">
+        <h1> second Page</h1>
+      </div>
+      <div className="three">
+        <h1> third Page</h1>
+      </div>
+      <div className="four">
+        <h1> fourth Page</h1>
+      </div>
+      <div className="five">
+        <h1> Fifth Page</h1>
+      </div>
+    </div>
+  );
+};
+export default Homepage;
+
+/*
+<div className="container">
+      <section className="one">
+        <h1> First Page</h1>
+      </section>
+      <section className="two">
+        <h1> second Page</h1>
+      </section>
+      <section className="three">
+        <h1> third Page</h1>
+      </section>
+      <section className="four">
+        <h1> fourth Page</h1>
+      </section>
+      <section className="five">
+        <h1> Fifth Page</h1>
+      </section>
+    </div>
+    */
