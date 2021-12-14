4edc31e67a76d67f74afabed5a43de8239ee7f0e
acharanusha99 <anusha.acharya@robosoftin.com>
2021-12-14 16:00:02 +0530

    converted SCSS styling to styled components

---
+++ src/variables.tsx
@@ -0,0 +1,53 @@
+export const colors = {
+  white: "#ffffff",
+  transparentColor: "transparent",
+  pink1: "rgba(226, 17, 67, 0.9)",
+  pink2: "rgba(255, 51, 102, 0.8)",
+  yellow1: "rgba(255, 176, 58, 0.9)",
+  yellow2: "rgba(255, 176, 58, 0.8)",
+  grey1: "rgba(151, 151, 151, 0.19)",
+  grey2: "rgba(99, 99, 100, 0.7)",
+  grey3: "rgba(151, 151, 151, 1)",
+  grey4: "rgba(99, 99, 100, 0.7)",
+};
+
+export const sizes = {
+  fullWidth: "100%",
+  halfWidth: "50%",
+  width35p: "35%",
+  size0: "0",
+  size1: "1px",
+  size227: "227px",
+  size19_4: "19.4px",
+  size19: "19px",
+  size2: "2px",
+  size14: "14px",
+  size50: "50px",
+  size60: "60px",
+  size17: "17px",
+  size103: "103px",
+  size9: "9px",
+  size10: "10px",
+  size24: "24px",
+  size22_41: "22.41px",
+  size24_95: "24.95px",
+  size28: "28px",
+};
+
+export const angles = {
+  angle60: "60deg",
+};
+
+export const fontFamilies = {
+  fontFamilyBn: "BebasNeue-Regular",
+  fontFamilyOs: "OpenSans-Light",
+  fontFamilyOsSemiBold: "OpenSans-SemiBold",
+};
+
+export const fontWeight = {
+  weight600: "600",
+};
+
+export const opacity = {
+  opacity0_19: 0.19,
+};
