b2108d4cf24111c3b3d68f05ae0db194296ee53d
RakeshKamath <rakeshrkamath19@gmail.com>
2022-01-04 18:43:44 +0530

    changes done

--- src/containers/create-account/index.tsx
+++ src/containers/create-account/index.tsx
@@ -13,7 +13,6 @@ import {
   fontWeight,
   links,
 } from "../../variables";
-
 const CreateAccount = ({
   onCloseModal,
   onOpenModal,
--- src/containers/verified/index.tsx
+++ src/containers/verified/index.tsx
@@ -13,7 +13,6 @@ import {
   fontWeight,
   links,
 } from "../../variables";
-
 const Verified = ({
   onCloseModal,
   onOpenModal,
