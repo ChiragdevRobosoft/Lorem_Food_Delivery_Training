6e6c348c4c7af1cd2b265696ad471311d23ac09b
rakeshrkamath <95073435+rakeshrkamath@users.noreply.github.com>
2022-01-18 19:41:29 +0530

    Profile screen and converted hardcode in 3 files (#17)
    * Profile screen and converted hardcode in 3 files
    
    * label component imported and some changes made in other files
    
    * label component changed
    
    * label component changed
    
    Co-authored-by: RakeshKamath <rakeshrkamath19@gmail.com>

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,10 +3,10 @@ import Home from "../components/home";
 import RestaurantList from "../components/Restaurant list";
 import RestaurantDetails from "../components/RestaurantDetails";
 import CartDataProvider from "../components/common/CartDataProvider";
+import Profile from "../components/profile";
 import Menu from "../components/RestaurantDetails/Menu";
 import Overview from "../components/RestaurantDetails/Overview";
 import { links } from "../variables";
-import AddressCard from "../components/common/addressCard";
 
 const Routing = () => {
   return (
@@ -14,6 +14,7 @@ const Routing = () => {
       <Routes>
         <Route path={links.home} element={<Home />} />
         <Route path={links.restaurantList} element={<RestaurantList />} />
+        <Route path={links.profile} element={<Profile />} />
         <Route path={links.restaurantDetails} element={<RestaurantDetails />}>
           <Route path={links.menu} element={<Menu />} />
           <Route path={links.overview} element={<Overview />} />
