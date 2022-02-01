f9691505dbc56e609d0b3b84307d6c546946a272
acharanusha99 <95067787+acharanusha99@users.noreply.github.com>
2022-02-01 14:47:03 +0530

    Restaurant details responsive (#40)
    * responsiveness
    
    * responsiveness of search component
    
    * responsiveness
    
    * removed unused imports
    
    Co-authored-by: acharanusha99 <“anusha.acharya@robosoftin.com”>

--- src/components/RestaurantDetails/index.tsx
+++ src/components/RestaurantDetails/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styled from "styled-components";
-import { colors, sizes, fontFamilies, angles } from "../../variables";
+import styled, { css } from "styled-components";
+import { colors, sizes, fontFamilies } from "../../variables";
 import Searchpath from "../common/Searchpath";
 import data from "../common/constants.json";
 import BgImg from "../../assets/restaurantDetails/BgImage.png";
@@ -15,6 +15,7 @@ import Foodsearch from "../common/SearchComponent";
 import Navbar from "../common/Navbar";
 import Image from "../common/image";
 import Label from "../common/label";
+import { queries } from "../common/breakpoints";
 
 const RestaurantDetails = () => {
   const restaurant = JSON.parse(localStorage.getItem("restaurant") || "{}");
@@ -28,7 +29,7 @@ const RestaurantDetails = () => {
           <BgImage src={BgImg} />
         </BgImageContainer>
         <ImageContentSection>
-          <Image src={RestaurantImg} />
+          <RestaurantImage src={RestaurantImg} />
           <CutleryImage src={Cutlery} />
           <RestaurantName content={restaurant.name} />
           <FoodVariety content={data.restaurantData.foodVariety} />
@@ -69,6 +70,17 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
+const RestaurantImage = styled(Image)`
+  ${queries.tabletLandscape} {
+    width: 768px;
+    height: auto;
+  }
+  ${queries.tablet} {
+    width: 375px;
+    height: 150px;
+  }
+`;
+
 const MainContainer = styled.div`
   width: ${sizes.fullWidth};
   background: ${colors.violet_f1f3fb};
@@ -84,20 +96,51 @@ const ImageContentSection = styled.div`
   right: 0;
   margin: auto;
   top: 83px;
+  display: fex;
+  flex-direction: column;
+  ${queries.tabletLandscape} {
+    width: 768px;
+    height: auto;
+  }
+  ${queries.tablet} {
+    width: 375px;
+    height: 150px;
+    top: 63px;
+  }
 `;
 
 const BgImageContainer = styled.div`
   height: 376px;
+  ${queries.tabletLandscape} {
+    height: 304px;
+  }
+  ${queries.tablet} {
+    height: 169px;
+  }
 `;
 
 const BgImage = styled(Image)`
-  width: 100%;
+  max-width: 100%;
+  height: 267px;
+  ${queries.tablet} {
+    height: 130px;
+  }
 `;
 
 const CutleryImage = styled(Image)`
   position: absolute;
   top: 20px;
   left: 39px;
+  ${queries.tabletLandscape} {
+    width: 60px;
+    height: auto;
+  }
+  ${queries.tablet} {
+    width: 30px;
+    height: auto;
+    left: 10px;
+    top: 10px;
+  }
 `;
 
 const RestaurantName = styled(Label)`
@@ -109,6 +152,19 @@ const RestaurantName = styled(Label)`
   line-height: ${sizes.size36};
   top: 125px;
   left: 39px;
+  ${queries.tabletLandscape} {
+    font-size: ${sizes.size22};
+    line-height: ${sizes.size26};
+    height: ${sizes.size26};
+    top: 100px;
+  }
+  ${queries.tablet} {
+    font-size: ${sizes.size12};
+    line-height: ${sizes.size14};
+    height: ${sizes.size14};
+    left: 10px;
+    top: 40px;
+  }
 `;
 
 const FoodVariety = styled(Label)`
@@ -119,10 +175,22 @@ const FoodVariety = styled(Label)`
   line-height: ${sizes.size19};
   top: 168px;
   left: 39px;
+  ${queries.tabletLandscape} {
+    font-size: ${sizes.size12};
+    line-height: ${sizes.size14};
+    height: ${sizes.size14};
+    top: 135px;
+  }
+  ${queries.tablet} {
+    font-size: ${sizes.size9};
+    line-height: ${sizes.size11};
+    height: ${sizes.size11};
+    left: 10px;
+    top: 60px;
+  }
 `;
 
 const Rating = styled(Label)`
-  position: absolute;
   height: 23px;
   width: 42px;
   border-radius: ${sizes.size4};
@@ -133,7 +201,12 @@ const Rating = styled(Label)`
   line-height: ${sizes.size20};
   text-align: center;
   justify-content: center;
-  left: 39px;
+  ${queries.tablet} {
+    width: 20px;
+    height: auto;
+    font-size: ${sizes.size9};
+    line-height: ${sizes.size11};
+  }
 `;
 
 const DetailsContainer = styled.div`
@@ -145,26 +218,58 @@ const DetailsContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  left: ${(props) => (props.className === "first-row" ? "0px" : "41px")}; ;
+  left: ${(props) => (props.className === "first-row" ? "39px" : "41px")};
+  ${(props) =>
+    props.className === "first-row" &&
+    css`
+      ${queries.tabletLandscape} {
+        top: 160px;
+      }
+      ${queries.tablet} {
+        top: 70px;
+        left: 10px;
+      }
+    `};
+  ${(props) =>
+    props.className !== "first-row" &&
+    css`
+      ${queries.tabletLandscape} {
+        top: 225px;
+      }
+      ${queries.tablet} {
+        top: 120px;
+        left: 10px;
+        width: 110px;
+      }
+    `}
 `;
 
 const DetailsList = styled.ul`
   display: flex;
   flex-direction: row;
-  height: 17px;
   gap: 25px;
   padding: 0;
-  margin-left: 111px;
+  margin-left: 20px;
   margin-right: 16px;
+  ${queries.tablet} {
+    gap: 15px;
+  }
 `;
 
 const Detail = styled.li`
+  height: 17px;
   color: ${colors.grey_757575_9};
   font-family: ${fontFamilies.fontFamilyOsRegular};
   font-size: ${sizes.size12};
   letter-spacing: ${sizes.size0};
   line-height: ${sizes.size17};
   text-indent: -3px;
+  ${queries.tablet} {
+    font-size: ${sizes.size9};
+    line-height: ${sizes.size11};
+    height: ${sizes.size11};
+    text-indent: -9px;
+  }
 `;
 
 const Time = styled(Label)`
@@ -174,6 +279,11 @@ const Time = styled(Label)`
   font-size: ${sizes.size12};
   line-height: ${sizes.size17};
   margin-left: 6.5px;
+  ${queries.tablet} {
+    font-size: ${sizes.size9};
+    line-height: ${sizes.size11};
+    height: ${sizes.size11};
+  }
 `;
 
 const RatingTitle = styled(Label)`
@@ -187,16 +297,38 @@ const RatingTitle = styled(Label)`
   left: 41px;
   padding-bottom: 6px;
   border-bottom: ${sizes.size2} solid ${colors.white_ededed};
+  ${queries.tabletLandscape} {
+    top: 190px;
+  }
+  ${queries.tablet} {
+    top: 100px;
+    left: 10px;
+    font-size: ${sizes.size9};
+    line-height: ${sizes.size12};
+    height: ${sizes.size12};
+  }
 `;
 
 const NavbarConatiner = styled.div`
   width: 972px;
   margin: 37px auto 11px auto;
+  ${queries.tabletLandscape} {
+    width: 768px;
+  }
+  ${queries.tablet} {
+    width: 375px;
+  }
 `;
 
 const RoutingContainer = styled.div`
   width: 972px;
   margin: auto;
+  ${queries.tabletLandscape} {
+    width: 768px;
+  }
+  ${queries.tablet} {
+    width: 375px;
+  }
 `;
 
 export default RestaurantDetails;
